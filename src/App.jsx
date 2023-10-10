953c8ec62e9aa748af2cb6552292d87cd4c8cf5f
Stan <stan@stansa.dev>
2023-10-10 12:49:48 -0700

    Initial init with basic being interviewed page structure

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const codeAreaDefVal = `\
+  function f(A) {
+    // your code here
+  }
+`
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="flex flex-row w-full h-full justify-around appBg">
+      <div className="biMainLayoutCol">
+        <div className="biTranscriptCont">
+          <div className="biTranscriptBg">
+          </div>
+          <div className="absolute z-50">
+            AI Transcript
+          </div>
+        </div>
+      </div>
+      <div className="biMainLayoutCol w-2/5">
+        <div className="w-full h-4/5 flex flex-col justify-between">
+          <div className="w-full h-1/4 mb-10 border-solid border-black">
+            <h2>Question 1</h2>
+            <p>Write an algorithm which sorts an array of integers from least to greatest in O(NlogN) time and O(1) space (in-place).</p>
+            <p>Example Input: A = [1, 5, 2, 3, 10]</p>
+            <p>Example Output: nothing (sort in place)</p>
+          </div>
+          <form onSubmit={e => e.preventDefault()} className="w-full h-4/5 flex flex-col justify-between">
+            <div className="w-full h-4/5 border-solid border-black">
+              <textarea defaultValue={codeAreaDefVal} className="w-full h-full bg-slate-700 opacity-50"></textarea>
+            </div>
+            <div className="w-full h-1/8 border-solid border-black">
+              <button className="w-1/2 h-full bg-slate-700 opacity-50">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="biMainLayoutCol">
+        <div className="biTranscriptCont">
+          <div className="biTranscriptBg">
+          </div>
+          <div className="absolute z-50">
+            Your Transcript
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
